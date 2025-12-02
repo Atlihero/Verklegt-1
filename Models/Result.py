112483979e3c4628b04bf6c2db4f62125b4910d0
ElvarEldjarn <elvareldjarn@gmail.com>
2025-12-02 14:02:21 +0000

    Update Result.py

---
+++ Models/Result.py
@@ -0,0 +1,15 @@
+from Models.Game import Game
+
+class result:
+    "Klasi fyrir score_A, score_B og winner "
+    def __init__(self, score_A, score_B, winner):
+        self.score_A = score_A
+        self.score_B = score_B
+        self.winner = winner
+    
+    "Strengur sem skilar þeim úpllýsingum í nýjum línum"
+    def __str__(self, score_A, score_B, winner):
+        return f"{score_A}:\n{score_B}:\n{winner}"
+
+    def __repr__(self):
+        return str(self)
