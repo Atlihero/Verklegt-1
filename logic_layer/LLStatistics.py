b21375c3068e8198209205c5f8094058eb8f7b31
Atlihero <atlithh25@ru.is>
2025-12-02 17:04:25 +0000

    Update LLStatistics.py

---
+++ logic_layer/LLStatistics.py
@@ -0,0 +1,27 @@
+#Data maniger !!!!
+
+#calculate team stats
+#calculate player stats
+#calculate club stats = B recuerment
+    #hvert lið tilheyrir club
+    # hefur einstakt nafn
+    # hefur sitt land og heimabæ
+
+    # !! tveir clubs ekki sama nafn og úr sama bæ
+
+def calculate_team_stats():
+    """stig eftir sigur mót og fram..
+    í hvaða mótum hann hefur tekið þátt í
+"""
+    pass
+
+def calculate_player_stats():
+    """player skorar mark == stats eða sigur == stats og fram. 
+    fyrir hvaða lið hann hefur spilað ef hann hefur skipt um lið
+    fyrir hvaða klúbba hann hefur spilað fyrir"""
+    pass
+
+def calculate_club_stats():
+    """mörk hjá club og sigur
+    hefur sitt land og heimabæ"""
+    pass
