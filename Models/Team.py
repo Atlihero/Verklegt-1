91c4540b039972b01bffc62412ea93f5ec33c469
DanielDarri <danieldarri2005@gmail.com>
2025-12-02 15:41:22 +0000

    Update Team.py

--- Models/Team.py
+++ Models/Team.py
@@ -1,14 +1,14 @@
 from Models.Player import Player
 
 class Team:
-    "Klasi fyrir nafn, kaftein, leikmenn og ASCII logo á lið "
+    #Klasi fyrir nafn, kaftein, leikmenn og ASCII logo á lið
     def __init__(self, name, captain, players, asciiLogo):
         self.name = name
         self.captain = captain
         self.players = players
         self.asciiLogo = asciiLogo
     
-    "Strengur sem skilar þeim úpllýsingum í nýjum línum"
+    #Strengur sem skilar þeim úpllýsingum í nýjum línum
     def __str__(self, name, captain, players, asciiLogo):
         return f"{name}:\n{captain}:\n{players}:\n{asciiLogo}"
 
