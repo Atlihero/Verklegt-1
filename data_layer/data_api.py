db11057ffb051ad9118eb04c633b98c3695dda72
DanielDarri <danieldarri2005@gmail.com>
2025-12-02 17:40:04 +0000

    Update data_api.py

---
+++ data_layer/data_api.py
@@ -0,0 +1,21 @@
+#Hér á að koma föll sem ná í rétta data
+from Models.Player import Player
+from Models.Team import Team
+
+def get_players():
+    "sækir í leikmenn"
+    pass
+
+def add_player():
+    "bætir við leikmanni"
+    pass
+
+def get_team():
+    "Sækir í lið"
+    pass
+
+def add_team():
+    "Bætir við liði úr create team í LL"
+    pass
+
+
