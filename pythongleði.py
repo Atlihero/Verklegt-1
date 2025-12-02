b357070ec4c14ad0524302e8b2002d61aa694d73
brynjaloa <brynjaloa@gmail.com>
2025-12-02 12:46:27 +0000

    Create pythongleði.py

---
+++ pythongleði.py
@@ -0,0 +1,18 @@
+
+# mkdir {data, ui, logig}_layer
+# búa til 3 layers
+'''
+búa til git ignore og setja .DS_Store & __pycache__/ file inn í það
+getur verið vesen skrá fyrir 
+__pycache__/ , / gefur til kynna að þetta er mappa
+'''
+
+'''
+mkdir models
+e models/MenuItem.py '''
+
+
+# hafa docstrings fyrir aftan hvern class
+# þarf að vera __init__.py skrá í hverri möppu
+
+
