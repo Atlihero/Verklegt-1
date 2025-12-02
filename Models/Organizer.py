a8442fca8a93f1e313aae311e069a607df4515e3
DanielDarri <danieldarri2005@gmail.com>
2025-12-02 14:52:13 +0000

    Update Organizer.py

---
+++ Models/Organizer.py
@@ -0,0 +1,7 @@
+class Organizer():
+    def __init__(self, adminLogin: str ="", name: str =""):
+        self.adminLogin = adminLogin
+        self.name = name 
+    
+    def __str__(self):
+        return f"Organizer(adminLogin='{self.adminLogin}', name='{self.name}')"
