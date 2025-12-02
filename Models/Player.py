9807293942342058c138a113fc4dc0f914401558
DanielDarri <danieldarri2005@gmail.com>
2025-12-02 10:38:36 +0000

    Update Player.py

---
+++ Models/Player.py
@@ -0,0 +1,16 @@
+class Player():
+    
+    def __init__(self, name, dob, address, email, handle, link):
+        self.name = name
+        self.dob = dob
+        self.address = address
+        self.email = email
+        self.handle = handle
+        self.link = link
+
+    def __str__(self, name, dob, address, email, handle, link):
+        return f"{name}:\n\t{dob}:\n\t{address}:\n\t{email}:\n\t{handle}:\n\t{link}"
+        
+    def name():
+        pass
+        
