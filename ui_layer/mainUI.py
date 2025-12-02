6ea2f08511008d4cad9a8bfd30174fdfcdd9c8c2
DanielDarri <danieldarri2005@gmail.com>
2025-12-02 18:53:10 +0000

    Update mainUI.py

---
+++ ui_layer/mainUI.py
@@ -0,0 +1,14 @@
+class MenuUI:
+    pass
+
+class CaptainUI:
+    pass
+
+class OrganizerUI:
+    pass
+
+class PublicViewer:
+    pass
+
+class Player: #Ekki alveg viss um þennan af því að það er bara eitt use case fyrir player
+    pass
