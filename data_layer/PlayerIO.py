bce362a41ccc5af2469537fb164f9c93f298ceab
DanielDarri <danieldarri2005@gmail.com>
2025-12-03 13:59:21 +0000

    Update PlayerIO.py

--- data_layer/PlayerIO.py
+++ data_layer/PlayerIO.py
@@ -5,6 +5,14 @@ PLAYER_PATH: str = r"_data\Players.csv"
 #Class for all the player info 
 class PlayerIO:
 
+    def get_players():
+        players = []
+        with open(PLAYER_PATH, "r", encoding="utf-8") as csvfile:
+            reader  = csvfile.readlines()
+            for row in reader:
+                players.append(row)
+        return players
+
     def add_new_player(player: list):
         with open(PLAYER_PATH, "a",newline="", encoding="utf-8") as csvfile:
             writer = csv.writer(csvfile)
