a5109ea795f2be40a5851cbe8c8a38a97a0ddab0
DanielDarri <danieldarri2005@gmail.com>
2025-12-03 10:23:47 +0000

    Update Team.py

--- Models/Team.py
+++ Models/Team.py
@@ -1,16 +1,13 @@
-from Models.Player import Player
-
-class Team(Player):
+class Team:
     #Klasi fyrir nafn, kaftein, leikmenn og ASCII logo á lið
-    def __init__(self, name, captain, players, asciiLogo):
+    def __init__(self, name, captain, asciiLogo):
         self.name = name
         self.captain = captain
-        self.players = players
         self.asciiLogo = asciiLogo
     
     #Strengur sem skilar þeim úpllýsingum í nýjum línum
-    def __str__(self, name, captain, players, asciiLogo):
-        return f"{name}:\n{captain}:\n{players}:\n{asciiLogo}"
+    def __str__(self, name, captain, asciiLogo):
+        return f"{name}:\n{captain}:\n{asciiLogo}"
 
     def __repr__(self):
         return str(self)
