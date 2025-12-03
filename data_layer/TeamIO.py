00e988ad5a69d17d8868116b6f239535f578640c
DanielDarri <danieldarri2005@gmail.com>
2025-12-03 15:46:52 +0000

    Update TeamIO.py

--- data_layer/TeamIO.py
+++ data_layer/TeamIO.py
@@ -1,8 +1,17 @@
+import csv
+
+TEAM_PATH: str = r"_data\Teams.csv"
 class TeamIO:
 
+
+
     def get_team():
-        "Sækir í lið til að skoða"
-        pass
+        Teams = []
+        with open(TEAM_PATH, "r", encoding="utf-8") as csvfile:
+            reader  = csvfile.readlines()
+            for row in reader:
+                Teams.append(row)
+        return Teams
 
     def add_new_team():
         "bætir við liði sem user býr til"
