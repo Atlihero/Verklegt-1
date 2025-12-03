cd71758610c90efaeb3b9df4a090d613dec93d3e
DanielDarri <danieldarri2005@gmail.com>
2025-12-03 13:43:05 +0000

    Skipta data layers upp

---
+++ data_layer/TournamentIO.py
@@ -0,0 +1,16 @@
+class TournamentIO:
+
+    def get_tournaments():
+        "Sækir í mót"
+        pass
+
+    def add_new_tournament():
+        "Bætir við nýju móti"
+        pass
+
+    def get_games():
+        "Nær í leiki í mótinu !!Líklegt breyting hér!!"
+        pass
+
+    def get_round():
+        "Nær í rounds í mótinu !!Líklegt breyting hér!!"
