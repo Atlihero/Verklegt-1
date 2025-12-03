1ca9a464cc5931a3ac02078145d472f38dd443c1
brynjaloa <brynjaloa@gmail.com>
2025-12-03 14:23:03 +0000

    skrifa komment

--- logic_layer/LLPlayers.py
+++ logic_layer/LLPlayers.py
@@ -1,10 +1,23 @@
 from Models.Player import Player
 
+class PlayerInfo():
+        
+        def player_information():
+                # 6 input línur
+                pass
 
-def edit_player():
-        "fyrirliði vill breyta upllýsingum leikmanns í liði síns"
-        pass
+        def edit_player():
+                "fyrirliði vill breyta upllýsingum leikmanns í liði síns"
+                pass
 
-def create_player():
-        "Skipuleggjandi vill búa til leikmann"
-        pass
+        def create_player():
+                "Skipuleggjandi vill búa til leikmann"
+                pass
+
+
+
+
+# eitt fall fyrir það sem player slær inn
+        # def player_information
+# muna eftir að hafa villuboð
+# 
