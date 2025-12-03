f9c4c315ada00e7e9b63b29ee78dcb9e431f29e0
DanielDarri <danieldarri2005@gmail.com>
2025-12-03 13:54:00 +0000

    Laga

--- data_layer/data_IO.py
+++ data_layer/data_IO.py
@@ -5,25 +5,7 @@ PLAYER_PATH: str = r"_data\Players.csv"
 TEAM_PATH: str = r"_data\Teams.csv"
 
 #Klasi fyrir öllu sem tengist player í data layerinu
-class PlayerIO:
 
-    def get_players():
-        players = []
-        with open(PLAYER_PATH, "r", encoding="utf-8") as csvfile:
-            reader  = csvfile.readlines()
-            for row in reader:
-                players.append(row)
-        return players
-
-    def add_new_player(player: list):
-        with open(PLAYER_PATH, "a",newline="", encoding="utf-8") as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(player)
-        return True
-
-    def get_player_stats():
-        "sækir tölfræði leikmanna"
-        pass
 
     #Kannski meira    
 
