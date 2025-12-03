de2fc3448d8ea5bc5808c344148b641ccfcc2a0d
DanielDarri <danieldarri2005@gmail.com>
2025-12-03 14:45:09 +0000

    Update PlayerIO.py

--- data_layer/PlayerIO.py
+++ data_layer/PlayerIO.py
@@ -6,19 +6,28 @@ PLAYER_PATH: str = r"_data\Players.csv"
 class PlayerIO:
 
     def get_players():
-        players = []
-        with open(PLAYER_PATH, "r", encoding="utf-8") as csvfile:
-            reader  = csvfile.readlines()
-            for row in reader:
-                players.append(row)
-        return players
+        try:
+            players = [] #Empty list for the player you are getting
+            with open(PLAYER_PATH, "r", encoding="utf-8") as csvfile: #opens and reads the player.csv
+                reader  = csvfile.readlines()   #read the lines in the csv
+                for row in reader: #for loop that checks each row
+                    players.append(row) #appends the row of the selected input
+            return players #returns the "list" for the player that was selected
+        except ValueError: #in case of wrong inputs
+            f"Error message to be decided"
+
+
 
     def add_new_player(player: list):
-        with open(PLAYER_PATH, "a",newline="", encoding="utf-8") as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(player)
-        return True
+        try: 
+            with open(PLAYER_PATH, "a",newline="", encoding="utf-8") as csvfile:
+                writer = csv.writer(csvfile) #here the user writes in what is needed for the player like name DOB etc.
+                writer.writerow(player) #prints what was written in a new row
+            return f"New player added :)"    
+        except ValueError:   #in case of wrong inputs 
+            f"Error message to be decided"
+
 
     def get_player_stats():
-        
+
         pass
