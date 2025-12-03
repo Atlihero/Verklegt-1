15ebac26f240e7d300b697f71f68cd8feaeffc74
DanielDarri <danieldarri2005@gmail.com>
2025-12-03 10:04:55 +0000

    Update Organizer.py

--- Models/Organizer.py
+++ Models/Organizer.py
@@ -1,7 +1,9 @@
+#Klasi fyrir organizerinn sem gefur login hjá honum og nafn
 class Organizer():
     def __init__(self, adminLogin: str ="", name: str =""):
         self.adminLogin = adminLogin
         self.name = name 
     
+    #skila upllýsingunum
     def __str__(self):
-        return f"Organizer(adminLogin='{self.adminLogin}', name='{self.name}')"
+        return f"{self.adminLogin} \n {self.name}"
