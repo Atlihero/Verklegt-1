c7524d74d84b7e58d39193a575223efb518e9380
DanielDarri <danieldarri2005@gmail.com>
2025-12-03 09:23:59 +0000

    Update mainUI.py

--- ui_layer/mainUI.py
+++ ui_layer/mainUI.py
@@ -1,51 +1,11 @@
-<<<<<<< Updated upstream
-=======
-#Föllin hér gætu kannski verið í Logic layer
-
 class CaptainUI:
-
-    def add_player():
-        "Fyrirliði vill bæta leikmanni í lið"
     pass
 
-    def edit_player():
-        "fyrirliði vill breyta upllýsingum leikmanns í liði síns"
-        pass
-
-    #kannski eitthvað aðeins fleira
-
 class OrganizerUI:
-
-    def create_player():
-        "Skipuleggjandi vill búa til leikmann"
-        pass
-    
-    def create_tournament():
-        "Skipuleggjandi vill búa til mót"
-        pass
-
-    def create_team():
-        "skipuleggjandi vill búa til lið"
-        pass
-
-    def select_captain():
-        "Skipuleggjandi vill velja fyriliða í liði"
-        pass
+    pass
 
 class PublicViewer:
-
-    def view_teams():
-        "áhorfandi vill skoða upplýsingar liða"
-        pass
-
-    def view_schedule():
-        "áhorfandi vill skoða dagskrá móta"
-        pass
-
-    def view_results():
-        "áhorfandi vill skoða úrslit leikja"
-        pass
+    pass
 
 class Player: #Ekki alveg viss um þennan af því að það er bara eitt use case fyrir player
     pass
->>>>>>> Stashed changes
