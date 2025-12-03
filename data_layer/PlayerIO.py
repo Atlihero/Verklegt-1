4d04e9535923b664078623fc1cfbac65a178a179
DanielDarri <danieldarri2005@gmail.com>
2025-12-03 13:56:46 +0000

    Laga

--- data_layer/PlayerIO.py
+++ data_layer/PlayerIO.py
@@ -1,18 +1,9 @@
 import csv
 
 PLAYER_PATH: str = r"_data\Players.csv"
-TEAM_PATH: str = r"_data\Teams.csv"
 
 #Class for all the player info 
 class PlayerIO:
-
-    def get_players():
-        players = [] #empty list for the player you are seeking
-        with open(PLAYER_PATH, "r", encoding="utf-8") as csvfile: 
-            reader  = csvfile.readlines()
-            for row in reader:
-                players.append(row)
-        return players
 
     def add_new_player(player: list):
         with open(PLAYER_PATH, "a",newline="", encoding="utf-8") as csvfile:
--- data_layer/TeamIO.py
+++ data_layer/TeamIO.py
@@ -1,8 +1,16 @@
+import csv
+
+TEAM_PATH: str = r"_data\Teams.csv"
+
 class TeamIO:
 
-    def get_team():
-        "Sækir í lið til að skoða"
-        pass
+    def get_players():
+        players = [] #empty list for the player you are seeking
+        with open(TEAM_PATH, "r", encoding="utf-8") as csvfile: 
+            reader  = csvfile.readlines()
+            for row in reader:
+                players.append(row)
+        return players
 
     def add_new_team():
         "bætir við liði sem user býr til"
