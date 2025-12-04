fb4479574dc8c0882959762e730ad823baf00c34
brynjaloa <brynjaloa@gmail.com>
2025-12-04 10:33:19 +0000

    búa til fall sem tékkar email addressu

--- logic_layer/LLPlayers.py
+++ logic_layer/LLPlayers.py
@@ -33,21 +33,50 @@ class PlayerInfo():
         def player_phone():
                 pass
                 # phone_number = input("Enter player's phone number: ")
+                # þarf að vera 7 tölustafir
                 # try:
 
                 # except ValueError:
                 #         print("Invalid phone number. Try again.")
 
         def player_email():
+                '''Asks the player for an email address and checks if it is valid so he can continue
+                adding his information. Prints error messages when the input is not up to standards.'''
+
                 player_email = input("Enter the player's email address: ")
-                pass
-                # 6 input línur
+                try:
+                        local_name, domain = player_email.split("@")
+                                
+                except ValueError:
+                        if player_email.count("@") > 1:
+                                print("Email can only have 1 '@'. Please try again.")
+                        else:
+                                print("Email has to contain '@'. Please try again.")
+                        return PlayerInfo.player_email()
+                
+                if not local_name:
+                        print("Local part (name) may not be empty. Please try again")
+                        return PlayerInfo.player_email()
+                
+                domain_parts = domain.split(".")
+                if len(domain_parts) < 2 or not domain_parts[0] or not domain_parts[1]:
+                        print("Domain must have a name and a valid ending. Please try again.")
+                        return PlayerInfo.player_email()
+                
+                ending = domain.split(".")[-1]
+                if len(ending) < 2 or len(ending) > 3 or not ending.isalpha():
+                        print("The email must contain a valid ending. Please try again.")
+                        return PlayerInfo.player_email()
+       
+                return f"{player_email} is a valid email"
+
         def player_handle():
-                handle = input("Enter player's handle: ")
+                pass
+                #handle = input("Enter player's handle: ")
                         # búa til lista
                         # 
 
-
+        
         def edit_player():
                 "fyrirliði vill breyta upplýsingum leikmanns í liði síns"
                 pass
@@ -64,5 +93,5 @@ class PlayerInfo():
 # muna eftir að hafa villuboð
 # 
 
-player = PlayerInfo.dob_player()
+player = PlayerInfo.player_email()
 print(player)
