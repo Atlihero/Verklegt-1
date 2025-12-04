3dff0a0b1f2c75c8ddd6f6cd33c046df22063db5
Atlihero <atlithh25@ru.is>
2025-12-04 10:59:39 +0000

    UI imports

--- ui_layer/CaptainUI.py
+++ ui_layer/CaptainUI.py
@@ -1,2 +1,7 @@
+from logic_layer.LL_api import LL_api
+from logic_layer.LLPlayers import LLPlayers
+from logic_layer.LLTeams import LLTeams
+
+
 class CaptainUI:
     def __init__(self, )
---
+++ ui_layer/mainUI.py
@@ -0,0 +1,11 @@
+from logic_layer.LL_api import LL_api
+from logic_layer.LLPlayers import LLPlayers
+from logic_layer.LLStatistics import LLStatistics
+from logic_layer.LLTeams import LLTeams
+from logic_layer.LLTournament import LLTournament
+from ui_layer.CaptainUI import CaptainUI
+from ui_layer.OrganizerUI import OrganizerUI
+from ui_layer.Puplicviewer import Puplicviewer
+
+class MainUI:
+    def __init__(self):
