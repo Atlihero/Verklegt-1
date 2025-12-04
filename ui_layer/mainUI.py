fddd8c4aa6a295ff70f31dbeb15971b542e8ede6
Atlihero <atlithh25@ru.is>
2025-12-04 11:33:49 +0000

    Update mainUI.py

--- ui_layer/mainUI.py
+++ ui_layer/mainUI.py
@@ -1,11 +1,13 @@
-from logic_layer.LL_api import LL_api
+from logic_layer.LL_api import LLAPI
 from logic_layer.LLPlayers import LLPlayers
 from logic_layer.LLStatistics import LLStatistics
 from logic_layer.LLTeams import LLTeams
 from logic_layer.LLTournament import LLTournament
-from ui_layer.CaptainUI import CaptainUI
-from ui_layer.OrganizerUI import OrganizerUI
-from ui_layer.Puplicviewer import Puplicviewer
 
 class MainUI:
-    def __init__(self):
+    def __init__(self, LLAPI):
+        self.api = LLAPI
+        self.players = LLPlayers()
+        self.statistics = LLStatistics()
+        self.teams = LLTeams()
+        self.tournaments = LLTournaments()
