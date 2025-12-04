1c65f942c46c10d28d69e29545ef65169218f762
DanielDarri <danieldarri2005@gmail.com>
2025-12-04 09:29:41 +0000

    get tourney

--- DanniDebug.py
+++ DanniDebug.py
@@ -8,13 +8,15 @@ while True:
     print("3. get team")
     print("4. get player stats")
     print("5. Get team stats")
+    print("6. Get tournament")
 
-    val = input("Veldu verkefni (1-5): ")
+    val = input("Veldu verkefni (1-6): ")
 
 
     #Klasi fyrir öllu sem tengist player í data layerinu sem inheritar model klasan "player"
     PLAYER_PATH: str = r"data_layer\_data\Players.csv"
     TEAM_PATH: str = r"data_layer\_data\Teams.csv"
+    TOURNAMENT_PATH: str = r"data_layer\_data\Tournament.csv"
 
     
     if val == "1":
@@ -80,3 +82,21 @@ while True:
         Wins, Points = TeamIO.get_team_stats() 
         print(f"{Wins[userinput]} Wins")
         print(f"{Points[userinput]} Points")
+
+    
+    if val == "6":
+        userinput = int(input("Sláðu inn númer móts: "))
+        class TouranmentIO:
+            def get_tournaments():
+                try:
+                    Tournament = [] #Empty list in which the tournament that is chosen goes into
+                    with open(TOURNAMENT_PATH, "r", encoding="utf-8") as csvfile:
+                        reader  = csvfile.readlines() #reads the lines in the csv
+                        for row in reader: #for loop that goes through the lines to look for the right tournament
+                            Tournament.append(row) #append the tournament chosen to the list
+                        return Tournament #returns the list
+                except ValueError: #in case of wrong inputs
+                    return f"Error message to be decided"
+                
+        Tournament = TouranmentIO.get_tournaments()
+        print(Tournament[userinput])
--- data_layer/TournamentIO.py
+++ data_layer/TournamentIO.py
@@ -1,10 +1,19 @@
 import csv
 
+TOURNAMENT_PATH: str = r"_data\Tournament.csv"
+
 class TournamentIO:
 
     def get_tournaments():
-        "Sækir í mót"
-        pass
+        try:
+            Tournament = [] #Empty list in which the tournament that is chosen goes into
+            with open(TOURNAMENT_PATH, "r", encoding="utf-8") as csvfile:
+                reader  = csvfile.readlines() #reads the lines in the csv
+                for row in reader: #for loop that goes through the lines to look for the right tournament
+                    Tournament.append(row) #append the tournament chosen to the list
+            return Tournament #returns the list
+        except ValueError: #in case of wrong inputs
+            return f"Error message to be decided"
 
     def add_new_tournament():
         "Bætir við nýju móti"
