130379b3e7f3e5080d7c8a1bbde602b417befedb
brynjaloa <brynjaloa@gmail.com>
2025-12-04 11:55:23 +0000

    hvað fyrirliði þarf

---
+++ logic_layer/LLCaptain.py
@@ -0,0 +1,10 @@
+
+
+# Velja leikmann úr lista og setja í sitt lið
+# Remove-a leikmann úr sínu liði
+# Max 5 í liði, captain + 4
+# Warning signs (t.d 'ertu viss um að þú viljir eyða úr liði?')
+# villa ef verið er að reyna að bæta við 6. leikmanni
+    # ef reynt er að taka út leikmann sem er ekki í liðinu
+    # ef reynt er að bæta við leikmanni sem er nú þegar í liði
+# getur séð allar upplýsingar um leikmenn í sínu liði (eftir að hafa bætt leikmanni í liðið)
