2371aa35e81925ac1e10c325a5395ac777afa0c5
brynjaloa <brynjaloa@gmail.com>
2025-12-04 17:31:25 +0000

    bæta og breyta

--- ui_layer/mainUI.py
+++ ui_layer/mainUI.py
@@ -1,5 +1,5 @@
 from logic_layer.LL_API import LLAPI
-from logic_layer.LLPlayers import LLPlayers
+from logic_layer.LLPlayers import PlayerLL
 from logic_layer.LLStatistics import LLStatistics
 from logic_layer.LLTeams import LLTeams
 from logic_layer.LLTournament import LLTournament
@@ -7,7 +7,7 @@ from logic_layer.LLTournament import LLTournament
 class MainUI:
     def __init__(self, LLAPI):
         self.api = LLAPI
-        self.players = LLPlayers()
+        self.players = PlayerLL()
         self.statistics = LLStatistics()
         self.teams = LLTeams()
         self.tournaments = LLTournament()
