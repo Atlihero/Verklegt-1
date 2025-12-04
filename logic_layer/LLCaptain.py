70dcba3528bfc95448050619fc6b6d4a374aaa13
karenjonsdottir <karen.jonsdottir05@gmail.com>
2025-12-04 16:19:59 +0000

    bætt við input

--- logic_layer/LLCaptain.py
+++ logic_layer/LLCaptain.py
@@ -7,13 +7,14 @@ class Captain_actions():
         # Input fyrir nafn
         # búa til liðslista max 5 á listanum
         # 'do you want to add this person to your team? Y/N'
-        # villa ef reynt við 6. manni
+        # villa ef reynt við 6. manni - Team is full
         # villa ef reynt er að bæta við manni sem er nú þegar í liði
     
     def remove_from_team(self):
+        remove_player = input("Please enter the name of the player you want to remove: ")
         # input fyrir nafn sem á að eyða úr liðslista
         # warning signs 'u sure u want to delete this person?'
-        # villa ef reynt er að taka út leikmann sem er ekki í liðinu
+        # villa ef reynt er að taka út leikmann sem er ekki í liðinu.
 
     def see_player_info(self):
         # getur séð allar upplýsingar um leikmenn í sínu liði (eftir að hafa bætt leikmanni í liðið)
