c8b0b91cdc5a7a660340700c2c584880109e9c9f
Atlihero <atlithh25@ru.is>
2025-12-04 11:10:28 +0000

    Update CaptainUI.py

--- ui_layer/CaptainUI.py
+++ ui_layer/CaptainUI.py
@@ -4,4 +4,10 @@ from logic_layer.LLTeams import LLTeams
 
 
 class CaptainUI:
-    def __init__(self, )
+    def __init__(self, select_captain, add_player, information_on_captain_team):
+        self.select_captain = select_captain
+        self.add_player = add_player
+        self.information_on_captain_team = information_on_captain_team
+
+    def __str__(self):
+        pass
