547bb8d2e0f28c5f9f97a7c816dd395c36ed5a3c
DanielDarri <danieldarri2005@gmail.com>
2025-12-04 16:59:23 +0000

    Update data_api.py

--- data_layer/data_api.py
+++ data_layer/data_api.py
@@ -2,22 +2,31 @@ from PlayerIO import PlayerIO
 from TeamIO import TeamIO
 
 def get_all_teams():
+    "Gets the teams from the csv to the LL"
     return TeamIO.get_team()
 
 def get_all_players():
+    "gets the players from the csv to the LL"
     return PlayerIO.get_players()
 
 def add_team(team_dict):
+    "adds a new team to the csv with the dict details"
     return TeamIO.create_new_team(team_dict)
 
 def add_player(player_dict):
+    "adds a new player to the csv with the dict data"
     return PlayerIO.create_new_player(player_dict)
 
 def get_team_statistics():
+    "Retrieves team statistics"
     return TeamIO.get_team_stats()
 
 def get_player_statistics():
+    "retrieves the player statistics"
     return PlayerIO.get_player_stats()
 
+
+#Pæling að breyta þessum
 def add_teams_to_tournament(tournament_name, teams_list):
+    "add exactly 16 teams to the tournament"
     return TeamIO.add_teams_to_tournament(tournament_name, teams_list)
