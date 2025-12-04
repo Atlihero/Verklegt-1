08a263bec08537622be3a76183fbb2764df42fde
DanielDarri <danieldarri2005@gmail.com>
2025-12-04 16:53:52 +0000

    data api komið eitthvað

--- data_layer/data_api.py
+++ data_layer/data_api.py
@@ -1,4 +1,23 @@
-from data_IO import PlayerIO
+from PlayerIO import PlayerIO
+from TeamIO import TeamIO
 
-class DataAPI:
-    pass
+def get_all_teams():
+    return TeamIO.get_team()
+
+def get_all_players():
+    return PlayerIO.get_players()
+
+def add_team(team_dict):
+    return TeamIO.create_new_team(team_dict)
+
+def add_player(player_dict):
+    return PlayerIO.create_new_player(player_dict)
+
+def get_team_statistics():
+    return TeamIO.get_team_stats()
+
+def get_player_statistics():
+    return PlayerIO.get_player_stats()
+
+def add_teams_to_tournament(tournament_name, teams_list):
+    return TeamIO.add_teams_to_tournament(tournament_name, teams_list)
