6662a368e3e54f0e67b2753530a42ae3717bf6e1
DanielDarri <danieldarri2005@gmail.com>
2025-12-05 10:32:17 +0000

    Update data_api.py

--- data_layer/data_api.py
+++ data_layer/data_api.py
@@ -8,48 +8,48 @@ class DataAPI:
         self.player = PlayerIO()
         self.team = TeamIO()
 
-def get_all_Teams(team_list):
-    "Gets all the teams an puts them in a lsit"
-    return TeamIO.get_all_teams(team_list)
+    def get_all_Teams(self):
+        "Gets all the teams an puts them in a list"
+        return self.team.get_all_teams()
 
-def get_teams():
-    "Gets the teams from the csv to the LL"
-    return TeamIO.get_team()
+    def get_teams(self):
+        "Gets the teams from the csv to the LL"
+        return self.team.get_team()
 
-def get_all_players():
-    "gets the players from the csv to the LL"
-    return PlayerIO.get_players()
+    def get_all_players(self):
+        "gets the players from the csv to the LL"
+        return self.player.get_players()
 
-def add_team(team_dict):
-    "adds a new team to the csv with the dict details"
-    return TeamIO.create_new_team(team_dict)
+    def add_team(self,team_dict):
+        "adds a new team to the csv with the dict details"
+        return self.team.add_new_team(team_dict)
 
-def add_player(player_dict):
-    "adds a new player to the csv with the dict data"
-    return PlayerIO.create_new_player(player_dict)
+    def add_player(self,player_dict):
+        "adds a new player to the csv with the dict data"
+        return self.add_player(player_dict)
 
-def get_team_statistics():
-    "Retrieves team statistics"
-    return TeamIO.get_team_stats()
+    def get_team_statistics(self):
+        "Retrieves team statistics"
+        return self.get_team_statistics()
 
-def get_player_statistics():
-    "retrieves the player statistics"
-    return PlayerIO.get_player_stats()
+    def get_player_statistics(self):
+        "retrieves the player statistics"
+        return self.get_player_statistics()
 
 
-#Pæling að breyta þessum
-def add_teams_to_tournament(tournament_name, teams_list):
-    "add exactly 16 teams to the tournament"
-    return TeamIO.add_teams_to_tournament(tournament_name, teams_list)
+    #Pæling að breyta þessum
+    def add_teams_to_tournament(self, tournament_name, teams_list):
+        "add exactly 16 teams to the tournament"
+        return self.add_teams_to_tournament(tournament_name, teams_list)
 
-def new_tournament():
-    "Creates a new tournament in the csv"
-    return TournamentIO.create_new_tournament()
+    def new_tournament(self):
+        "Creates a new tournament in the csv"
+        return self.new_tournament
 
-def get_all_tournaments():
-    "retrieves the tournaments from the csv"
-    return TournamentIO.get_tournaments()
+    def get_all_tournaments(self):
+        "retrieves the tournaments from the csv"
+        return self.get_all_tournaments()
 
-def new_game():
-    "creates a new game in the csv"
-    return TournamentIO.create_new_game()
+    def new_game(self):
+        "creates a new game in the csv"
+        return self.new_game()
