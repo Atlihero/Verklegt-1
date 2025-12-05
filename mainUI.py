cf09220b0ca7a81f2fe4e5586439fc86d0d11979
Atlihero <atlithh25@ru.is>
2025-12-05 21:01:17 +0000

    Update mainUI.py

--- mainUI.py
+++ mainUI.py
@@ -1,14 +1,15 @@
 from logic_layer.LL_api import LL_API
 from Colors import Colers
 
-class Main:
-    def menu():
+class Happy_paths:
+    count = 0
+    def Happy_menu():
         print("\033[91m--------------------------------------\033[0m")
         print("\033[91m---           GLADIATORS           ---\033[0m")
         print("\033[91m--------------------------------------\033[0m")
-        print("\033[92m---    #Organizer                  ---\033[0m")
-        print("\033[92m---    #Captain                    ---\033[0m")
-        print("\033[92m---    #Puplic viewer              ---\033[0m")
+        print("\033[92m---    #Organizer(1)               ---\033[0m")
+        print("\033[92m---    #Captain(2)                 ---\033[0m")
+        print("\033[92m---    #Puplic viewer(3)           ---\033[0m")
         print("\033[92m---                                ---\033[0m")
         print("\033[92m---    #Please select you option   ---\033[0m")
         print("\033[92m---      (1,2,3 or b for back)     ---\033[0m")
@@ -17,23 +18,67 @@ class Main:
         print("\033[96m--------------------------------------\033[0m")
         print("\033[96m--- Back b)                        ---\033[0m")
         print("\033[96m--------------------------------------\033[0m")
-        fyrst_input = 8 #This code will change just testing
-        while fyrst_input != "1" and fyrst_input != "2" and fyrst_input != "3" and fyrst_input !="b":
-            print("Please select a valid valiu")
-            fyrst_input = input("")
-        if fyrst_input != "1" and fyrst_input != "2" and fyrst_input != "3" and fyrst_input !="b":
-            quit
-        elif fyrst_input == "1" or fyrst_input == "2" or fyrst_input == "3" or fyrst_input == "b":
-            print("nice")
-
-    def orginazer(self):
-        self.api =LL_API
-        
-
-    menu()
-
-    def scoobster(pink, reset, blue, neonblue, Yellow, gray, red, highlite, neongreen):
-        print(pink,"########################")
-        print(pink,"## ",highlite,"--Scooby doo--",reset,pink,"##")
     
-    scoobster(*Colers.all_colers())
+    def Happy_organizer():
+        print("\033[91m--------------------------------------\033[0m")
+        print("\033[91m---           GLADIATORS           ---\033[0m")
+        print("\033[91m--------------------------------------\033[0m")
+        print("\033[93m---   >Selected Organizer          ---\033[0m")
+        print("\033[92m---                                ---\033[0m")
+        print("\033[92m---    #Create player(1)           ---\033[0m")
+        print("\033[92m---    #Create tournament(2)       ---\033[0m")
+        print("\033[92m---    #Create team(3)             ---\033[0m")
+        print("\033[92m---    #Create a captain(4)        ---\033[0m")
+        print("\033[92m---    (1,2,3,4 or b for back)     ---\033[0m")
+        print("\033[96m--------------------------------------\033[0m")
+        print("\033[96m---     #Pleas enter your input    ---\033[0m")
+        print("\033[96m--------------------------------------\033[0m")
+        print("\033[96m--- Back b)                        ---\033[0m")
+        print("\033[96m--------------------------------------\033[0m")
+
+
+    def Happy_create_player():
+        print("\033[91m--------------------------------------\033[0m")
+        print("\033[91m---           GLADIATORS           ---\033[0m")
+        print("\033[91m--------------------------------------\033[0m")
+        print("\033[93m---   >Selected create player      ---\033[0m")
+        print("\033[92m---                                ---\033[0m")
+        print("\033[92m---    #Input players information  ---\033[0m")
+        print("\033[92m---                                ---\033[0m")
+        print("\033[92m---    #Name                       ---\033[0m")
+        print("\033[92m---    #Date of birth              ---\033[0m")
+        print("\033[92m---    #email                      ---\033[0m")
+        print("\033[92m---    #Phone                      ---\033[0m")
+        print("\033[92m---    #gamertag                   ---\033[0m")
+        print("\033[96m--------------------------------------\033[0m")
+        print("\033[96m--- Back b)                        ---\033[0m")
+        print("\033[96m--------------------------------------\033[0m")
+
+
+    Happy_menu()
+    fyrst_input = "" #This code will change just testing
+    while fyrst_input != "1" and fyrst_input != "2" and fyrst_input != "3" and fyrst_input !="b":
+        print("Please select a valid valiu")
+        fyrst_input = input("")
+    if fyrst_input != "1" and fyrst_input != "2" and fyrst_input != "3" and fyrst_input !="b":
+        quit
+    elif fyrst_input == "1":
+        count = 1
+        Happy_organizer()
+    elif fyrst_input in ["2","3","b"]:
+        print("nice")
+
+    if count == 1:
+        input2 = input("")
+        while input2 != "1" and input2 != "2" and input2 != "3" and input2 !="4" and input2 != "b":
+            print("Please select a valid valiu")
+        if input2 == "1":
+            Happy_create_player()
+
+
+
+def scoobster(pink, reset, blue, neonblue, Yellow, gray, red, highlite, neongreen):
+    print(pink,"########################")
+    print(pink,"## ",highlite,"--Scooby doo--",reset,pink,"##")
+
+scoobster(*Colers.all_colers())
