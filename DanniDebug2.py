c47d6485b79ef8ac64e35658c86af2cc51caa8a3
DanielDarri <danieldarri2005@gmail.com>
2025-12-05 16:19:35 +0000

    d

--- DanniDebug2.py
+++ DanniDebug2.py
@@ -1,11 +1,7 @@
 from logic_layer.LL_api import LL_API
 
-PLAYER_PATH = r"data_layer\_data\Players.csv"
-TEAM_PATH = r"data_layer\_data\Teams.csv"
-TOURNAMENT_PATH = r"data_layer\_data\Tournament.csv"
-GAMES_PATH = r"data_layer\_data\Games.csv"
 
-userinput = int(input("Veldu ID leikmanns: "))
+userinput = int(input("Veldu ID leikmanns milli 1-57: "))
 def getplayer():
     api = LL_API()        
     players = api.getplayers()
