bb27721db3ef138a54b37358397de04c141a66b9
DanielDarri <danieldarri2005@gmail.com>
2025-12-05 10:18:28 +0000

    Update data_api.py

--- data_layer/data_api.py
+++ data_layer/data_api.py
@@ -2,32 +2,39 @@ from PlayerIO import PlayerIO
 from TeamIO import TeamIO
 from TournamentIO import TournamentIO
 
-def get_all_teams():
-    return TeamIO.get_team()
 
-def get_all_players():
-    return PlayerIO.get_players()
+class DataAPI:
+    def __init__(self):
+        self.tournament = TournamentIO()
+        self.player = PlayerIO()
+        self.team = TeamIO()
 
-def add_team(team_dict):
-    return TeamIO.create_new_team(team_dict)
+    def get_teams():
+        return TeamIO.get_team()
 
-def add_player(player_dict):
-    return PlayerIO.create_new_player(player_dict)
+    def get_all_players():
+        return PlayerIO.get_players()
 
-def get_team_statistics():
-    return TeamIO.get_team_stats()
+    def add_team(team_dict):
+        return TeamIO.create_new_team(team_dict)
 
-def get_player_statistics():
-    return PlayerIO.get_player_stats()
+    def add_player(player_dict):
+        return PlayerIO.create_new_player(player_dict)
 
-def add_teams_to_tournament(tournament_name, teams_list):
-    return TeamIO.add_teams_to_tournament(tournament_name, teams_list)
+    def get_team_statistics():
+        return TeamIO.get_team_stats()
 
-def new_tournament():
-    return TournamentIO.create_new_tournament()
+    def get_player_statistics():
+        return PlayerIO.get_player_stats()
 
-def get_all_tournaments():
-    return TournamentIO.get_tournaments()
+    def add_teams_to_tournament(tournament_name, teams_list):
+        return TeamIO.add_teams_to_tournament(tournament_name, teams_list)
 
-def new_game():
-    return TournamentIO.create_new_game()
+    def new_tournament():
+        return TournamentIO.create_new_tournament()
+
+    def get_all_tournaments():
+        return TournamentIO.get_tournaments()
+
+    def new_game():
+        return TournamentIO.create_new_game()
