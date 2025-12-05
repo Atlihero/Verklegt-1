a6bb4370ddb59c73c65069c13d98fb502e290162
DanielDarri <danieldarri2005@gmail.com>
2025-12-05 09:48:36 +0000

    Create CaptainUI.py

---
+++ ui_layer/CaptainUI.py
@@ -0,0 +1,15 @@
+from logic_layer.LL_api import LL_api
+from logic_layer.LLPlayers import LLPlayers
+from logic_layer.LLTeams import LLTeams
+
+
+class CaptainUI:
+    def __init__(self, LLPlayers, LLTeams):
+        self.players = LLPlayers
+        self.teams = LLTeams
+
+    def add_player(self, p):
+        self.players.add_player(p)
+
+    def add_team(self, t):
+        self.teams.create_team(t)
