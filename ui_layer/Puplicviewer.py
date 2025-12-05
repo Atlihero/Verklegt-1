3526071e11827af5fe61e9396fc04fbf3d168296
DanielDarri <danieldarri2005@gmail.com>
2025-12-05 12:21:37 +0000

    Update Puplicviewer.py

--- ui_layer/Puplicviewer.py
+++ ui_layer/Puplicviewer.py
@@ -1,5 +1,12 @@
 from logic_layer.LL_api import LL_API
 
+
+"""
+Viewer vill sjá Lið og leikmenn
+og þá bara nöfn liða
+Handle leikmanns
+"""
+
 class PublicViewerUI(LL_API):
      
      def __init__(self):
