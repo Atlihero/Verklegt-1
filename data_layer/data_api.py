d1194db6abb1a397b963a1bb7ca176c72040d503
DanielDarri <danieldarri2005@gmail.com>
2025-12-05 11:13:43 +0000

    Update data_api.py

--- data_layer/data_api.py
+++ data_layer/data_api.py
@@ -10,7 +10,7 @@ class DataAPI:
 
     def get_all_Teams(self):
         "Gets all the teams an puts them in a list"
-        return self.team.get_all_teams()
+        return self.team.ge
 
     def get_teams(self):
         "Gets the teams from the csv to the LL"
@@ -22,34 +22,34 @@ class DataAPI:
 
     def add_team(self,team_dict):
         "adds a new team to the csv with the dict details"
-        return self.team.add_new_team(team_dict)
+        return self.team.create_new_team(team_dict)
 
     def add_player(self,player_dict):
         "adds a new player to the csv with the dict data"
-        return self.add_player(player_dict)
+        return self.player.create_new_player(player_dict)
 
     def get_team_statistics(self):
         "Retrieves team statistics"
-        return self.get_team_statistics()
+        return self.team.get_team_stats()
 
     def get_player_statistics(self):
         "retrieves the player statistics"
-        return self.get_player_statistics()
+        return self.player.get_player_stats()
 
 
     #Pæling að breyta þessum
-    def add_teams_to_tournament(self, tournament_name, teams_list):
+    def add_team_to_tournament(self, tournament_name, teams_list):
         "add exactly 16 teams to the tournament"
-        return self.add_teams_to_tournament(tournament_name, teams_list)
+        return self.team.add_teams_to_tournament(tournament_name, teams_list)
 
-    def new_tournament(self):
+    def new_tournament(self, tournament_list):
         "Creates a new tournament in the csv"
-        return self.new_tournament
+        return self.tournament.create_new_tournament(tournament_list)
 
     def get_all_tournaments(self):
         "retrieves the tournaments from the csv"
-        return self.get_all_tournaments()
+        return self.tournament.get_tournaments(self)
 
-    def new_game(self):
+    def new_game(self, game_list):
         "creates a new game in the csv"
-        return self.new_game()
+        return self.tournament.create_new_game(game_list)
