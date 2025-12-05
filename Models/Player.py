226faa861dd08421410c39a4846aa22084e70275
DanielDarri <danieldarri2005@gmail.com>
2025-12-05 09:53:34 +0000

    Update Player.py

--- Models/Player.py
+++ Models/Player.py
@@ -1,21 +1,33 @@
+from dataclasses import dataclass
+from datetime import date
+
 class Player:
     
     """
     Búa til __init__ fall sem tekur inn nafn, fæðingardag, heimili,
     email, viðurnefni(gamertag), og link að samfélagsmiðli
     """
-    def __init__(self, name="", dob="", address="", email="", handle="", link=""):
-        self.name = name
-        self.dob = dob
-        self.address = address
-        self.email = email
-        self.handle = handle
-        self.link = link
+    def __init__(self, name="", dob="", phone="", address="", email="", handle="", link=""):
+        self.name: str = name
+        self.dob: date = dob
+        self.phone: int = phone
+        self.address: str = address
+        self.email: str = email
+        self.handle: str = handle
+        self.link: str = link
 
-    "Skilar öllum upplýsingum leikmanns í nýrri línu"
     def __str__(self):
-        return f"{self.name}:\n{self.dob}:\n{self.address}:\n{self.email}:\n{self.handle}:\n{self.link}"
-    
+        dob_str = self.dob.strftime("%d/%m/%Y") if hasattr(self.dob, "strftime") else self.dob
+        
+        return (
+        f"Name    : {self.name}\n"
+        f"DOB     : {dob_str}\n"
+        f"Address : {self.address}\n"
+        f"Phone   : {self.phone}\n" 
+        f"Email   : {self.email}\n"
+        f"Handle  : {self.handle}\n"
+        f"Link    : {self.link}"
+        )
     def __repr__(self):
         return str(self)
         
