18ab0d52eb7fe5ebde5f4579eeb539fdb3cb1779
Atlihero <atlithh25@ru.is>
2025-12-05 15:55:53 +0000

    Update mainUI.py

--- ui_layer/mainUI.py
+++ ui_layer/mainUI.py
@@ -1,23 +1,61 @@
-from logic_layer.LL_api import LL_API
+#from logic_layer.LL_api import LL_API
 
-
-
-class MainUI:
-    def __init__(self):
-        self.api = LL_API()
+class Main:
+    def run():
         pass
 
-    def api_logic(self, api):
-        self.add_api(api)
+    def menu():
+        print("\033[91m--------------------------------------\033[0m")
+        print("\033[91m---           GLADIATORS           ---\033[0m")
+        print("\033[91m--------------------------------------\033[0m")
+        print("\033[92m---    #Organizer                  ---\033[0m")
+        print("\033[92m---    #Captain                    ---\033[0m")
+        print("\033[92m---    #Puplic viewer              ---\033[0m")
+        print("\033[92m---                                ---\033[0m")
+        print("\033[92m---    #Please select you option   ---\033[0m")
+        print("\033[92m---      (1,2,3 or b for back)     ---\033[0m")
+        print("\033[96m--------------------------------------\033[0m")
+        print("\033[96m---     #Pleas enter your input    ---\033[0m")
+        print("\033[96m--------------------------------------\033[0m")
+        print("\033[96m--- Back b)                        ---\033[0m")
+        print("\033[96m--------------------------------------\033[0m")
+        Fyrst_input = input("") #This code will change just testing
+        if Fyrst_input != "1" and "2" and "3" and "b":
+            quit
+        elif Fyrst_input == "1":
+            print("nice")
+    menu()
 
-    def add_player(self, players):
-        self.players.add_player(players)
 
-    def add_team(self, team):
-        self.teams.create_team(team)
+pink = "\033[95m"
+reset = "\033[0m"
+blue = "\033[94m"
+neonblue = "\033[96m"
+Yellow = "\033[93m"
+gray = "\033[90m"
+red = "\033[91m"
+highlite= "\033[100m"
+neongreen = "\033[92m"
 
-class CaptainUI:
-    def __init__(self):
+print(pink,"######################")
+print(pink,"## ",red,"------Wassup-----",pink,"##")
+
+
+#from logic_layer.LL_api import LL_api
+
+class Menu:
+    def menu():
+        print("\033[91mfjdkslfmdsnfs fsldkf skdfls jdfds8789745\033[0m")
+        print("\033[91m4875793kj34j5\033[0m")
+        print("\033[91m849348573k4j534...\033[0m")
+        print("\033[91mError 404 HAHA Just Kidding\033[0m")
+        print("\033[94mWelcome to test main menu\033[1m")
+        print("\033[96mpleas enter your bank account to continue ____:\033[0m")
+        val = input()
+        if val == 0:
+            pass
+        print("\033[91mIncorrect information\033[0m")
+    def run():
         pass
 
-
+    menu()
