b4166f74bbad986a265a67d0daf60b4ae24dce5c
Atlihero <atlithh25@ru.is>
2025-12-05 18:39:07 +0000

    Update OrganizerUI.py

--- OrganizerUI.py
+++ OrganizerUI.py
@@ -1,4 +1,18 @@
 from logic_layer.LL_api import LL_API
 
 class Orginizer:
-    def 
+    def create_player(self):
+        self.cr_player = LL_API.create_player()
+        return self.cr_player
+
+    def create_tournament(self):
+        self.cr_tournament = LL_API#wantae.tournament
+        return self.cr_tournament
+
+    def create_team(self):
+        self.cr_team = LL_API#wantar .create_team
+        return self.cr_team
+
+    def make_captain(self):
+        self.mk_captain = LL_API#wantar create captein
+        return self.mk_captain
