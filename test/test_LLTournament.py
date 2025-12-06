e1b900ff259ba57bf554351579f9fc5078384b25
chrissverrir <christian.s.lechler@gmail.com>
2025-12-06 13:35:15 +0000

    Update test_LLTournament.py

--- test/test_LLTournament.py
+++ test/test_LLTournament.py
@@ -1,17 +1,3 @@
-from logic_layer.LLTournament import TournamentService
-from data_layer.TeamIO import TeamIO
-from data_layer.PlayerIO import PlayerIO
-
-team_io = TeamIO()
-player_io = PlayerIO()
-service = TournamentService(team_io, player_io)
-
-print("Test OK, TournamentService created")
-
-
-
-# test_LLTournament.py
-
 from logic_layer.LLTournament import Tournament
 
 def main():
