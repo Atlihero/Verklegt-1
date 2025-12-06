08f9a283819d51b6e5679ab12acc10cef7c62b3b
chrissverrir <christian.s.lechler@gmail.com>
2025-12-06 15:53:18 +0000

    Update LLTournament.py

--- logic_layer/LLTournament.py
+++ logic_layer/LLTournament.py
@@ -1,24 +1,24 @@
-
-
 class Tournament():
+
+    #16 teams in the tournament
     def __init__(self, name : str ):
 
         self.tournament = name
-        self._next_game_id  = 1 
+        self._next_game_id  = 1
 
 
         self.teams = [ "Team1" , "Team2", "Team3 ", "Team4", "Team5", "Team6", "Team7", "Team8", 
                     "Team9", "Team10", "Team11", "Team12", "Team13", "Team14", "Team15", "Team16"]
 
-# CONTACT Information very Important , Also Updated 
-
-
+    # CONTACT Information 
     def get_contact_info(self, contact:str = "", email:str="", phone : str = ""):
         
         self.contact = contact 
         self.email = email 
         self.phone = phone 
 
+
+    # Updated Contact Information
     def update_contact_info(self, new_contact: str= "", new_email: str= "", new_phone : str = ""):
 
         if new_contact:
@@ -28,14 +28,13 @@ class Tournament():
         if new_phone:
             self.new_phone = new_phone
 
+    # Scheme of registering a team, a team name cant be twice in one Tournament 
     def register_team(self, team_name: str= "", players : list[str]= None):
         
         if self.find.team.by_name(team_name) is not None:
             raise ValueError(f"The team {team_name} already exists ")
         
         self.team_name = team_name
-
-
 
         if len(players) < 1: 
             raise ValueError ("A team must have at least 1 player ")
@@ -47,7 +46,7 @@ class Tournament():
 
         return team 
 
-
+    # the logic of scheduling games, 
     def schedule_games(self, start_date : str = "", end_date : str =  "", teams : list = 0, venue : str = ""):
         
         self.start_date = start_date 
@@ -57,14 +56,14 @@ class Tournament():
 
     # store all matches in a empty list 
 
-        self.matches =  []     # where all the matches are being stored 
-        self.rounds = {}       # where all the rounds are being stored 
+        self.matches =  []     # matches are stored 
+        self.rounds = {}       # rounds are stored 
 
 
 
         # requirements to schedule a game  
         if len(teams) < 2 : 
-            raise ValueError ("For a match there have to be at least 2 Teams")
+            raise ValueError ("For a match there have to be at least 2 Teams")    
         
         if not start_date:
             raise ValueError ("There is no start date given")
@@ -75,8 +74,7 @@ class Tournament():
         if not venue : 
             raise ValueError("There is no Venue to be played in")
         
-
-
+    # records results of games, final score, winner and player ids
     def record_result(self, game_result : int = 0, player_1id : str = "", player_2id : str = "", winner : str = "", outcome : str = "", score : str=  ""):
 
 
@@ -88,14 +86,13 @@ class Tournament():
         self.score = score 
 
 
-
     # The winner gets printed out 
         if self.winner != "":
             
             print(f"{self.winner} has won the game!")
 
 
-
+    # checks the length of the team names = strings, it is used for a "fake" scoring system for the def play_round
     def sum_logic(self, team_name: str) -> int:
         
         
@@ -115,34 +112,43 @@ class Tournament():
         self.status = status 
 
 
-# store all matches in a empty list 
+        # store all matches in a empty list 
 
         self.matches =  []     # where all the matches are being stored 
         self.rounds = {}       # where all the rounds are being stored 
 
 
-
-# brackets , round of 16, quartefinals, semifinals , final 
+        # brackets , round of 16, quartefinals, semifinals , final 
 
         if self.teams != 16 : 
             raise ValueError ("There has to be at least 16 teams in the Tournament!")
         
 
-    # splitting the tournament scheme into a left and right side
+        # split 16 teams into two sides (8 teams on the left, 8 teams on the right)        #!!! can be more than 16 teams 
+        left_teams = self.teams[:8]
+        right_teams = self.teams[8:]
 
-        left_teams = [ "team1", "team2", "team3" , "team4"
-                      "team5", "team6" , "team7" ,"team8"]
-        
+        # rounds of the left teams
 
-        right_teams = ["team9" ,"team10" ,"team11" ,"team12", 
-                       "team13" ,"team14", "team15", "team16"]
-    
-           
-        
+        round1_winners_left = self.play_round(left_teams) 
+        round2_winners_left = self.play_round(round1_winners_left)
+        round3_winners_left = self.play_round(round2_winners_left)
+        champion_left = round3_winners_left[0]
+        print(f"The winner from the left side is , {champion_left} !")
+
+    # rounds of the right teams
+
+        round1_winners_right = self.play_round(right_teams)
+        round2_winners_right = self.play_round (round1_winners_right)
+        round3_winners_right = self.play_round (round2_winners_right)
+        champion_right= round3_winners_right
+        print(f"The winner from the right side is , {champion_right}")
+
+
     def play_round(self, teams: list[str]) ->list [str]:
         winners = list[str] = []
 
-        # example this goes through the left teams , from 0 to 7 , in total 8 "left" teams
+        # 
 
         for  i in range (0, len(teams), 2):
              team1= teams[i]
@@ -161,54 +167,12 @@ class Tournament():
         
         return winners 
     
-
-    # implement left teams_list (team1 to team8)
-
-
-    left_teams = [ "team1", "team2", "team3" , "team4",
-                 "team5", "team6" , "team7" ,"team8"]
-    # left side 
-
-    # 8 teams play and 4 teams advance
-    round1_winners_left = play_round(left_teams) 
-
-    # 4 teams play and 2 teams advance
-    round2_winners_left = play_round(round1_winners_left)
-
-
-    # 2 teams play and 1 teams addvcanc etc 
-    round3_winners_left = play_round(round2_winners_left)
-
-    champion_left = round3_winners_left[0]
-
-    print(f"The winner from the left side is , {champion_left} !")
-
-
-
-    right_teams = [ "team9" ,"team10" ,"team11" ,"team12", 
-                  "team13" ,"team14", "team15", "team16"]
-
-    # right side 
-
-
-    round1_winners_right = play_round(right_teams)
-    
-    round2_winners_right = play_round (round1_winners_right)
-
-    round3_winners_right = play_round (round2_winners_right)
-
-    champion_right= round3_winners_right
-
-
-    print(f"The winner from the right side is , {champion_right}")
-
-
     # Úrslitaleikur / FINAL
 
-    finalists = play_round([champion_left, champion_right])
+    finalists = self.play_round([champion_left, champion_right])
 
     final_winner = finalists[0]
-    final_winner= print(f"The Tournamenyt winner is , {final_winner} !")
+    print(f"The Tournamenyt winner is , {final_winner} !")
 
 
 
