96652c3cee3dead244ce2577ec8df7e4fc83e869
brynjaloa <brynjaloa@gmail.com>
2025-12-07 20:55:09 +0000

    Create LLOrganizer.py

---
+++ logic_layer/LLOrganizer.py
@@ -0,0 +1,20 @@
+
+class LLOrganizer():
+
+    def create_tournament(self):
+        pass
+
+    def choose_date(self):
+        pass
+
+    def choose_location(self):
+        pass
+
+
+
+# create tournament
+    # skrá tengilið
+# choose location
+# choose start and end date
+# búa til leikjadagskrá
+# getur séð persónuupplýsingar
