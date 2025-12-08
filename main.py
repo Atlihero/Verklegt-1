0211d0add8d88c43b007ca9b41584583c4c2a9d1
Atlihero <atlithh25@ru.is>
2025-12-08 15:22:56 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from ui_layer.mainUI import Uimain
+
+def start_() -> Uimain:
+    main: Uimain = Uimain()
+    return main
+
+def main() -> None:
+    main: Uimain = start_()
+    main.start()
+
+
+if __name__ == "__main__":
+    main()
