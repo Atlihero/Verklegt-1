dfc17c527e71da0d1aa581e7f154e612f2d3136e
karenjonsdottir <karen.jonsdottir05@gmail.com>
2025-12-08 20:25:46 +0000

    updated OrganizerUI, make player

--- ui_layer/OrganizerUI.py
+++ ui_layer/OrganizerUI.py
@@ -1,7 +1,58 @@
 from logic_layer.LL_api import LL_API
 from Models.Tournament import Tournament
 
+
 class OrganizerUI():
+
+    def get_player_info():
+    
+        name = input("Enter full name of player: ")
+
+        while True:
+            dob_str = input("Enter player date of birth (DD/MM/YYYY): ")
+            try:
+                dob = LL_API.valid_dob(dob_str)
+                break
+            except ValueError as error:
+                print(error)
+            
+        address = input("Enter player's home address: ")
+    
+        while True:
+            phone_number = input("Enter player's phone number: ")
+            try:
+                LL_API.valid_phone(phone_number)
+                break
+            except ValueError as error:
+                print(error)
+
+        while True:
+            player_email = input("Enter the player's email address: ")
+            try: 
+                LL_API.valid_email(player_email)
+                break
+            except ValueError as error:
+                print(error)
+    
+        while True:
+            handle = input("Enter player's handle: ")
+            try:  
+                handle = LL_API.valid_handle(handle)
+                break
+            except ValueError as error:
+                print(error)
+
+        while True:
+            link = input("Enter a link (press 'Enter' to skip): ")
+            try:
+                link = LL_API.validate_link(link)
+                break
+            except ValueError as error:
+                print(error)
+
+        player = LL_API.create_player(name, dob, address, phone_number, player_email, handle, link)
+        print("Player created successfully!")
+        return player
 
     def createTournament(self):
         lapi = LL_API()
