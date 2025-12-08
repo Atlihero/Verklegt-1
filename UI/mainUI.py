2396a5f24a15d82cded0ff39328bd3b8b96f539e
Atlihero <atlithh25@ru.is>
2025-12-08 14:49:59 +0000

    fixing ui design

---
+++ UI/mainUI.py
@@ -0,0 +1,17 @@
+from logic_layer.LL_api import LL_API
+from UI.Happy_path import Happy_paths
+
+class Uimain:
+    """bleb blob"""
+   
+    def __init__(self) -> None:
+        """sup laddara"""
+        LL_api = LL_API()
+        self.paths = Happy_paths()
+
+    def __start__(self) -> None:
+        """blobmaxing"""
+        
+    
+        
+        
---
+++ main.py
@@ -0,0 +1,6 @@
+from UI.mainUI import Uimain
+
+def start_() -> Uimain:
+    main: Uimain = Uimain()
+    return main
+
