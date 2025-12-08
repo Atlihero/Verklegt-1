d0b4aa7e82df02f99df6132993c3ba49b52a0ba3
brynjaloa <brynjaloa@gmail.com>
2025-12-08 13:50:22 +0000

    Update LLTeams.py

--- logic_layer/LLTeams.py
+++ logic_layer/LLTeams.py
@@ -8,14 +8,15 @@ class LLTeams:
     def __init__(self): 
         self.teams: list[Team] = self._load_teams_from_csv()
 
+    
     def _load_teams_from_csv(self) -> list[Team]:
-        raw_rows = TeamIO.get_team()
+        raw_rows = TeamIO.get_team(self)
         teams: list[Team] = []
 
-        for line in raw_rows:  # Loops each line from the csv
+        for line in raw_rows: # Loops each line from the csv
             line = line.strip()
             if not line or line.startswith("TeamID"):
-                continue  # skip empty lines and header
+                continue # skip empty lines and header
 
             # splits on the comma and strips space + "
             parts = [p.strip(' "') for p in line.split(",")]
@@ -28,7 +29,7 @@ class LLTeams:
                 Team(
                     name=parts[1],       	
                     captain=parts[2],    	
-                    asciiLogo="",        	# Logo for later
+                    asciiLogo="",
                     wins = int(parts[3]),	
                     points = int(parts[4])	
                 )
@@ -36,8 +37,9 @@ class LLTeams:
 
         return teams
 
-    def add_player_to_team(self, team_name: str, player_name: str) -> Player:
-        "Captain wants to add a player to his team"
+    # per se þurfum ekki þetta fall, erum með í LLcaptain sem tékkar líka á fjölda í liði
+    '''def add_player_to_team(self, team_name: str, player_name: str) -> Player:
+        Captain wants to add a player to his team
 
         # Check if team exists
         team = self.get_team_by_name(team_name)
@@ -67,34 +69,36 @@ class LLTeams:
         # Save player list through PlayerIO
         PlayerIO.save_players(players)
 
-        return player_to_add
+        return player_to_add'''
 
+    
     def get_team_by_name(self, name: str) -> Team | None:
-        """Checks for the team and returns it if found, or None if not"""
+        '''Checks for the team and returns it if found, or None if not'''
         for team in self.teams:
             if team.name == name:
                 return team
         return None
 
+    
     def team_exists(self, name: str) -> bool:
         return self.get_team_by_name(name) is not None  # Checks if a team has this name
 
+    
     def create_team(self, name: str, captain: str, asciiLogo: str) -> Team:
-        "Organizer wants to create a team"
+        '''Organizer wants to create a team'''
         # Checks whether it has a name, captain and a unique name
         if not name.strip():
-            raise ValueError("Name can not be empty")
-        if not captain.strip():
-            raise ValueError("Captain can not be empty")
-        if self.team_exists(name):
-            raise ValueError("Another team already has this name")
+			raise ValueError("Name can not be empty. Please enter a name.")
+		if not captain.strip():
+			raise ValueError("Captain can not be empty. Please enter a name.")
+		if self.team_exists(name):
+			raise ValueError("A team with this name already exists. Please use another name.")
 
         # Creating team object
         new_team = Team(name=name.strip(), captain=captain.strip(), asciiLogo=asciiLogo)
 
         # Updating in memory
         self.teams.append(new_team)
-
         new_id = len(self.teams)
 
         # Saving in CSV through TeamIO
@@ -109,19 +113,21 @@ class LLTeams:
 
         return new_team
 
+    
     def select_captain(self, team_name: str, new_captain: str) -> Team:
-        "Organizer wants to chose a captain"
+		'''Organizer wants to add a captain to a team.'''
 
         team = self.get_team_by_name(team_name)
-        if team is None:
-            raise ValueError("Team with this name was not found")
-        if not new_captain.strip():
-            raise ValueError("The captain can not be empty")
+		if team is None:
+			raise ValueError("A team with this name was not found. Please make sure you have added the team before choosing a captain.")
+		if not new_captain.strip():
+			raise ValueError("Captain spot may not be empty. Please make sure that there is a captain for the team.")
+		
+		team.captain = new_captain.strip()
 
-        team.captain = new_captain.strip()
+		return team
 
-        return team
-
+    
     def view_teams(self) -> list[Team]:
-        "Spectator wants to see information about a team"
+		'''User wants to see information about teams in the tournament.'''
         return list(self.teams)
