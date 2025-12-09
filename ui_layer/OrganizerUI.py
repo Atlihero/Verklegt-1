0a89621609909d71b3ef705996320e4247728f69
brynjaloa <brynjaloa@gmail.com>
2025-12-09 18:37:41 +0000

    Update OrganizerUI.py

--- ui_layer/OrganizerUI.py
+++ ui_layer/OrganizerUI.py
@@ -1,6 +1,7 @@
 from logic_layer.LL_api import LL_API
 from Models.Tournament import Tournament
-lapi = LL_API()
+from Models.Player import Player
+
 
 def show_games(games, title="Current Games"):
     print(f"\n=== {title} ===")
@@ -8,69 +9,167 @@ def show_games(games, title="Current Games"):
         print(f"{g['match_number']:>2}: {g['round']} | {g['team_a']} vs {g['team_b']} | "
               f"Score: {g['score_a'] or '-'}-{g['score_b'] or '-'} | Winner: {g['winner'] or '-'}")
 
+
 class OrganizerUI():
-        def createTournament(self):
+
+    def __init__(self):
+        self.lapi = LL_API()
+
+
+    def get_player_info(self):
+    
+        while True:
+            name = input("Enter full name of player: ")
+            try:
+                name = self.lapi.valid_name(name)
+                break
+            except ValueError as error:
+                print(error)
+
+
+        while True:
+            dob_str = input("Enter player date of birth (DD/MM/YYYY): ")
+            try:
+                dob = self.lapi.valid_dob(dob_str)
+                break
+            except ValueError as error:
+                print(error)
             
-            while True:
-                unique_name = input("Create a unique name for the tournament: ")
-                try:
-                    unique_name = lapi.valid_tournament_name(unique_name)
-                    break
-                except ValueError as error:
-                    print(error)
+        while True:
+            address = input("Enter player's home address: ")
+            try:
+                address = self.lapi.valid_address(address)
+                break
+            except ValueError as error:
+                print(error) 
+        
+    
+        while True:
+            phone_number = input("Enter player's phone number: ")
+            try:
+                self.lapi.valid_phone(phone_number)
+                break
+            except ValueError as error:
+                print(error)
 
-            while True:
-                start_date = input("Select the start date of the tournament: ")
-                try:
-                    start_date = lapi.valid_start_date(start_date)
-                    break
-                except ValueError as error:
-                    print(error)
+        while True:
+            player_email = input("Enter the player's email address: ")
+            try: 
+                self.lapi.valid_email(player_email)
+                break
+            except ValueError as error:
+                print(error)
+    
+        while True:
+            handle = input("Enter player's handle: ")
+            try:  
+                handle = self.lapi.valid_handle(handle)
+                break
+            except ValueError as error:
+                print(error)
 
-            while True:
-                end_date = input("Select the end date of the tournament: ")
-                try:
-                    end_date = lapi.valid_end_date(end_date, start_date)
-                    break
-                except ValueError as error:
-                    print(error)
+        while True:
+            link = input("Enter a link (press 'Enter' to skip): ")
+            try:
+                link = self.lapi.validate_link(link)
+                break
+            except ValueError as error:
+                print(error)
 
+
+        player_obj = Player(
+            name = name,
+            dob = dob_str,
+            address = address,
+            phone = phone_number,
+            email = player_email,
+            handle = handle,
+            link = link,
+            team = None,
+            points = None
+        )
+
+        return self.lapi.create_player(player_obj)
+
+        #player = self.lapi.create_new_player(name, dob, address, phone_number, player_email, handle, link)
+        #print("Player created successfully!")
+        #return player
+
+    def createTournament(self):
+    
+        while True:
+            unique_name = input("Create a unique name for the tournament: ")
+            try:
+                unique_name = self.lapi.valid_tournament_name(unique_name)
+                break
+            except ValueError as error:
+                print(error)
+
+        while True:
+            start_date = input("Select the start date of the tournament: ")
+            try:
+                start_date = self.lapi.valid_start_date(start_date)
+                break
+            except ValueError as error:
+                print(error)
+
+        while True:
+            end_date = input("Select the end date of the tournament: ")
+            try:
+                end_date = self.lapi.valid_end_date(end_date, start_date)
+                break
+            except ValueError as error:
+                print(error)
+
+        while True:
             venue = input("Enter the name of a venue (location) for the tournament: ")
-                
-            contact_person = input("Name the contact person for this tournament: ")
-                
-            while True:
-                contact_email = input("What is the contact email for this tournament: ")
-                try: 
-                    lapi.valid_email(contact_email)
-                    break
-                except ValueError as error:
-                    print(error)
-                
-            while True:
-                contact_phone = input("What is the contact phone for this tournament: ")
-                try:
-                    lapi.valid_phone(contact_phone)
-                    break
-                except ValueError as error:
-                    print(error)
-                
-            # have it as an object not an dict so it is easier to return and read.
-            tournament_obj = Tournament( 
-                unique_name=unique_name,
-                start_date=start_date,
-                end_date=end_date,
-                venue=venue,
-                contact_person=contact_person,
-                contact_email=contact_email,
-                contact_phone=contact_phone
-            )
-            lapi.generateGames(unique_name, start_date)
-            return lapi.create_new_tournament(tournament_obj)
+            try:
+                venue = self.lapi.valid_tournament_location(venue)
+                break
+            except ValueError as error:
+                print(error)
+        
+        while True:
+            try:
+                contact_person = input("Name the contact person for this tournament: ")
+                contact_person = self.lapi.valid_tournament_contact(contact_person)
+                break
+            except ValueError as error:
+                print(error)
+        
+        while True:
+            contact_email = input("What is the contact email for this tournament: ")
+            try: 
+                self.lapi.valid_email(contact_email)
+                break
+            except ValueError as error:
+                print(error)
+        
+        while True:
+            contact_phone = input("What is the contact phone for this tournament: ")
+            try:
+                self.lapi.valid_phone(contact_phone)
+                break
+            except ValueError as error:
+                print(error)
+        
+    # have it as an object not an dict so it is easier to return and read.
+        tournament_obj = Tournament( 
+            unique_name=unique_name,
+            start_date=start_date,
+            end_date=end_date,
+            venue=venue,
+            contact_person=contact_person,
+            contact_email=contact_email,
+            contact_phone=contact_phone
+        )
+
+        return self.lapi.create_new_tournament(tournament_obj)
+
         
         def update_result(self):
             tournament_name = input("Enter tournament name: ").strip()
-            games = lapi.get_game_by_tournamentName(tournament_name)
+            games = self.lapi.get_game_by_tournamentName(tournament_name)
 
             if not games:
                 print("\nNo games found for this tournament.\n")
@@ -81,14 +180,14 @@ class OrganizerUI():
             score_a = int(input("Enter score for team A: "))
             score_b = int(input("Enter score for team B: "))
 
-            result = lapi.updateGame(match_number, score_a, score_b)
+            result = self.lapi.updateGame(match_number, score_a, score_b)
 
             winner = result["winner"]
             tournament_name = result["tournament_name"]
 
             # If a winner exists, advance
             if winner:
-                advance_result = lapi.advance_round(tournament_name, match_number, winner)
+                advance_result = self.lapi.advance_round(tournament_name, match_number, winner)
                 print(advance_result)
 
                 # ⭐ IF THIS WAS THE FINAL MATCH (F = match 15), SHOW WINNER BANNER
@@ -100,14 +199,14 @@ class OrganizerUI():
             else:
                 print("Game is a draw. Winner cannot advance.")
 
-            updated_games = lapi.get_game()
+            updated_games = self.lapi.get_game()
             show_games(updated_games, "Updated Games")
 
 
         def organizer_see_info(self) -> None:
             '''The organizer can see player information for every player in the tournament'''
             try:
-                players = self.ll.organizer_view_player_info()
+                players = self.lapi.organizer_view_player_info()
             except ValueError as error:
                 print("Error: ", error)
                 return
