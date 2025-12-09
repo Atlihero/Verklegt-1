ac79a358b3d2480ecdae9cd8d4b86874514486f5
Atlihero <atlithh25@ru.is>
2025-12-09 18:25:39 +0000

    ui upp

--- ui_layer/mainUI.py
+++ ui_layer/mainUI.py
@@ -3,6 +3,7 @@ from ui_layer.Happy_path import Happy_paths
 from ui_layer.OrganizerUI import OrganizerUI
 from ui_layer.Publicviewer import PublicViewer
 from ui_layer.CaptainUI import CaptainUI
+import os
 
 class Uimain:
     """This class suplies information from logic layer to its correct 
@@ -22,12 +23,30 @@ class Uimain:
         while True:
             paths.Happy_logo()
             user_inp = input("Press any button to start: ")
+            """Here the code starts"""
             if user_inp != 1:
                 paths.Happy_menu()
                 user_inp = input("Enter 1, 2, 3 or b: ")
+                
+                
+                """Here we select Organizer captain viewer"""
+
                 if user_inp == "1":
                     paths.Happy_organizer()
                     user_inp = input("Enter 1, 2, 3, 4 or b: ")
+                    if user_inp == "1":
+                        paths.Happy_create_player()
+                        exit()
+                elif user_inp == "2":
+                    paths.Happy_captain()
+                    user_inp = input("")
                     exit()
+                elif user_inp == "3":
+                    paths.Happy_viewer()
+                elif user_inp == "b":
+                    os.system('cls')
+                    continue
                 else:
                     exit()
+            else:
+                exit()
