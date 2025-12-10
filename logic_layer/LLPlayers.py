cbdc813f438c9194ce1aac8e67477cb756f472c5
DanielDarri <danieldarri2005@gmail.com>
2025-12-10 10:19:28 +0000

    catchup

--- logic_layer/LLPlayers.py
+++ logic_layer/LLPlayers.py
@@ -1,26 +1,38 @@
-from Models.Player import Player
+from Models import Player
 from datetime import datetime
 from data_layer.data_api import DataAPI
+from data_layer.PlayerIO import PlayerIO
 
-class LLPlayer:
-    # because no names can be the same, doesn't add if they are the same
-    existing_handles = set() 
+class LLPlayer():
 
-    
+    def __init__(self):
+        self.data = DataAPI()
+        self.playerio = PlayerIO
+
+
     def get_all_players(self):
-        data = DataAPI()
-        return data.get_all_players()
+        return self.data.get_all_players()
     
+
     def get_player_publicViewer(self):
-        data = DataAPI()
-        return data.public_get_player()
+        return self.data.public_get_player()
     
+
     def get_player_statistics(self):
-        data = DataAPI()
-        return data.get_player_statistics()
+        return self.data.get_player_statistics()
 
 
-    def validate_dob(dob_str: str) -> datetime: 
+    def validate_name(self, name: str) -> str:
+        '''Checks if name is unique or missing a name'''
+        # check if name is just empty, so just space or something
+        name = name.strip()
+        if not name:
+            raise ValueError("Player name cannot be emtpy. Please enter a valid name.")
+    
+        return name
+
+
+    def validate_dob(self, dob_str: str) -> datetime: 
         '''Checks players date of birth and if the format fits the 
         standards, then the user can continue inputting the information.'''
 
@@ -35,7 +47,18 @@ class LLPlayer:
         except ValueError:
             raise ValueError ("Invalid date. Use DD/MM/YYYY")
         
-    def validate_phone(phone_number: int) -> int:
+
+    def validate_address(self, address: str) -> str:
+        '''Checks if name is unique or missing a name'''
+        # check if name is just empty, so just space or something
+        address = address.strip()
+        if not address:
+            raise ValueError("Player's address name cannot be emtpy. Please enter a valid address.")
+        
+        return address
+        
+
+    def validate_phone(self, phone_number: int) -> int:
         '''Validates the players phone number, if not then 
           the user tries again.'''
 
@@ -45,7 +68,7 @@ class LLPlayer:
         return phone_number
             
 
-    def validate_email(player_email: str) -> str:
+    def validate_email(self, player_email: str) -> str:
         '''Checks players email address and if it is valid then he can contiue.
            Raises error messages when the input is not up to standards.'''
 
@@ -69,28 +92,50 @@ class LLPlayer:
         ending = domain_parts[-1]
         if len(ending) < 2 or len(ending) > 3 or not ending.isalpha():
             raise ValueError("The email must contain a valid ending. Please try again.")
-            
-        
+
         return player_email
 
-    def validate_handle(handle: str) -> str:
+
+    def validate_handle(self, handle: str) -> str:
         '''Checks players handle. It checks if the username 
         is already in use and then asks for a new username since no two players 
         can have the same username '''
         
-        if handle in LLPlayer.existing_handles:
-            raise ValueError("This handle is already taken. Please try another one.")
+        handle = handle.strip()
+        if not handle:
+            raise ValueError("Player's handle name cannot be emtpy. Please enter a handle.")
         
-		# if the handle is unique then its added to the list
-        LLPlayer.existing_handles.add(handle)
+        existing_usernames = self.data.get_all_players()
+        existing_handles = [p.handle for p in existing_usernames]
+        
+        if handle in existing_handles:
+            raise ValueError("Handle is already in use, please choose another one.")
+
         return handle
+
     
-    def validate_link(link: str) -> str:
+    def validate_link(self, link: str) -> str:
         if not link:
             return "" # user didn't add a link
         if not (link.startswith("http://") or link.startswith("https://")):
             raise ValueError("Link must start with 'http://' or 'https://'. Please try another link")
         return link
     
-    def create_player(name, dob_str, address, phone_number, player_email, player_handle, link):
-        return Player.Player(name, dob_str, address, phone_number, player_email, player_handle, link)
+    #def create_player(self, name, dob_str, address, phone_number, player_email, player_handle, link):
+    #    return Player.Player(name, dob_str, address, phone_number, player_email, player_handle, link)
+
+    def create_player(self, player_obj: Player) -> Player:
+    
+        player_list = [ 
+            player_obj.name,
+            player_obj.dob,
+            player_obj.address,
+            player_obj.phone,
+            player_obj.email,
+            player_obj.handle,
+            player_obj.team,
+            player_obj.points,
+            player_obj.link
+        ]
+
+        return self.playerio.create_new_player(player_list)
