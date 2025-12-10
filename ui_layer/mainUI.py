a8c9ac1f4d38bdbff5515c9b18df67da0a4f9bba
chrissverrir <christian.s.lechler@gmail.com>
2025-12-10 09:35:14 +0000

    Update mainUI.py

--- ui_layer/mainUI.py
+++ ui_layer/mainUI.py
@@ -9,5 +9,96 @@ class Uimain:
         LL_api = LL_API()
         self.paths = Happy_paths()
 
+
+#Happy Menu 
+
     def start(self) -> None:
         """blobmaxing"""
+        while True: 
+                    self.paths = Happy_paths
+
+    user_input = input("> ").strip().lower()
+
+                if user_input == "1":
+                    print("You selected Organizer (1)")
+                # here you could call another menu or logic
+                elif user_input == "2":
+                    print("You selected Captain (2)")
+                elif user_input == "3":
+                    print("You selected Public viewer (3)")
+                elif user_input == "b":
+                    print("Exiting menu...")
+
+# Happy Viewer 
+
+
+    def start(self)-> None:
+            
+        
+        user_input = input(">").strip().lower()
+          
+
+            if user_input == "1":
+                  print("You selected Information on the team ")
+            elif user_input == "2":
+                  print("You selected an Schedule")
+            elif user_input == "3":
+                  print("You selected the Result")
+
+
+
+# there is nothing in Happy Captain right now
+
+
+
+
+
+
+
+
+
+    
+# Happy Organizer 
+    def start(self)-> None: 
+          
+
+
+          user_input  = input(">").strip().lower()
+
+
+            if user_input == "1":
+                  print("You selected to create an Player") == "1"
+            elif user_input == "2":
+                  print("You selected an Create Tournament")== "2"
+            elif user_input == "3":
+                  print("You selected create an Tournament")
+            elif user_input == "4":
+                  print("Make Captain")
+            elif user_input == "b":
+                  print("You selected back")
+
+    
+
+
+                  
+#  nothing in Happy Create Tournament 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Happy cretae 
+
+
+
+
