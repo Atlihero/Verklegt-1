6490df1e11ce065e9d689a31d15a8c8c175cc483
DanielDarri <danieldarri2005@gmail.com>
2025-12-10 10:08:07 +0000

    Update mainUI.py

--- ui_layer/mainUI.py
+++ ui_layer/mainUI.py
@@ -1,15 +1,68 @@
 from logic_layer.LL_api import LL_API
+from ui_layer.Happy_path import Happy_paths
+from ui_layer.OrganizerUI import OrganizerUI
+from ui_layer.Publicviewer import PublicViewer
+from ui_layer.CaptainUI import CaptainUI
+import os
+
+class Uimain:
+    """This class suplies information from logic layer to its correct 
+    location and gives restrictions depending of user input"""
+    def __init__(self) -> None:
+        """gets logic_layer as a variable"""
+        LL_api = LL_API()
+        self.captain = CaptainUI()
+        self.publicviewer = PublicViewer()
+        self.organizer = OrganizerUI()
+        
 
 
-class MainUI:
-    def __init__(self):
-        self.api = LL_API()
+    def start(self) -> None:
+        """Prints the game screen on terminal and controles the flow of information"""
+        paths = Happy_paths
+        organizer = OrganizerUI
+        while True:
+            paths.Happy_logo()
+            user_inp = input("Press any button to start: ")
+            """Here the code starts"""
+            if user_inp != 1:
+                paths.Happy_menu()
+                user_inp = input("Enter 1, 2, 3 or b: ")
+                
+                
+                """Here we select Organizer or captain or viewer"""
 
-    def api_logic(self, api):
-        self.add_api(api)
-
-    def add_player(self, players):
-        self.players.add_player(players)
-
-    def add_team(self, team):
-        self.teams.create_team(team)
+                if user_inp == "1":
+                    """Now we are in Organizer and can chose what we will do there"""
+                    paths.Happy_organizer() #This is the organizer
+                    user_inp = input("Enter 1, 2, 3, 4 or b: ")
+                    if user_inp == "1": #Here you creata a new player
+                        paths.Happy_create_player()
+                        organizer
+                        exit()
+                    elif user_inp == "2": #Here you create a tournament
+                        paths.Happy_create_tournament()
+                        exit()
+                    elif user_inp == "3": #Here you create a team
+                        paths.Happy_create_team()
+                        exit()
+                    elif user_inp == "4": #here you make a player a captain
+                        paths.Happy_make_captain()
+                    elif user_inp == "b": #return back to starting psition
+                        os.system('cls')
+                        continue
+                elif user_inp == "2": #This is the Captain
+                    paths.Happy_captain()
+                    user_inp = input("")
+                    exit()
+                elif user_inp == "3": #This is the puplic viewer
+                    paths.Happy_viewer()
+                elif user_inp == "b": #return back to the start
+                    os.system('cls')
+                    continue
+                else: # if you enter an invalid input
+                    print("invalid input")
+                    #need to find a way to return to input not back or exit
+                    exit()
+            else: #stops the code if you some how manige to give int not string which should not be possible
+                exit()
