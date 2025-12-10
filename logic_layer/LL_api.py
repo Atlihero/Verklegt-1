d7cfe915a0137c4d3f989a461668a4ea45546f60
DanielDarri <danieldarri2005@gmail.com>
2025-12-10 19:14:27 +0000

    Laga

--- logic_layer/LL_api.py
+++ logic_layer/LL_api.py
@@ -128,6 +128,9 @@ class LL_API:
         '''Determines what team advances to the next round of the tournament'''
         return self.tournament.advance(tournament_name ,match_number, winner)
 
+    def get_game_by_tournamentName(self, tournament_name):
+        games = self.tournament.get_all_games()
+        return [g for g in games if g["tournament_name"] == tournament_name]
 
     '''Logic layer wrapper for the LLCaptain'''
 
--- ui_layer/OrganizerUI.py
+++ ui_layer/OrganizerUI.py
@@ -180,7 +180,7 @@ class OrganizerUI():
         score_a = int(input("Enter score for team A: "))
         score_b = int(input("Enter score for team B: "))
 
-        result = self.lapi.updateGame(tournament_name, match_number, score_a, score_b)
+        result = self.lapi.update_game(tournament_name, match_number, score_a, score_b)
 
         winner = result["winner"]
         tournament_name = result["tournament_name"]
@@ -198,7 +198,7 @@ class OrganizerUI():
             print("Game is a draw. Winner cannot advance.")
 
 
-        updated_games = self.lapi.get_game()
+        updated_games = self.lapi.get_game_by_tournamentName(tournament_name)
         show_games(updated_games, "Updated Games")
 
 
