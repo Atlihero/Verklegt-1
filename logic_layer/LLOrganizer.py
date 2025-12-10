2d844f14945d976dbe24c22be814dbbf82b5297c
brynjaloa <brynjaloa@gmail.com>
2025-12-10 19:01:34 +0000

    Update LLOrganizer.py
    comments og samræma setup

--- logic_layer/LLOrganizer.py
+++ logic_layer/LLOrganizer.py
@@ -11,41 +11,42 @@ class LLOrganizer():
 
     def tournament_name(self, name: str) -> str:
         '''Checks if name is unique or missing a name'''
-        # check if name is just empty, so just space or something
+        
         name = name.strip()
-        if not name:
+        if not name: # Check for empty inputs
             raise ValueError("Tournament name cannot be emtpy. Please enter a valid name.")
         
+        # Get all tournament names from the csv file
         existing_tournament_names = self.dapi.get_all_tournaments()
         existing_name = [row[0] for row in existing_tournament_names if row]
-        if name in existing_name:
+        if name in existing_name: # Checking if the name is unique
             raise ValueError("Name already exists, please choose another one.")
 
         return name
        
 
     def choose_start_date(self, start_date: str) -> datetime:
-        '''Check if the start_date for a tournament is a valid date'''
+        '''Check if the start date for a tournament is a valid date'''
         try:
-            # change string input to datetime
+            # Change string input to datetime
             start_date = datetime.strptime(start_date, "%d/%m/%Y")
             
-			# check if inputted date is in the future, then not valid
+			# Check if inputted date is in the future, if not in the future then invalid date
             if start_date < datetime.now():
                 raise ValueError("Date has to be in the future. Please enter a valid date.")
             return start_date
-        except ValueError:
+        except ValueError: # If the formatting is wrong
             raise ValueError ("Invalid date. Use DD/MM/YYYY")
 
     
     def choose_end_date(self, end_date, start_date) -> datetime:
         '''Check if the end date for a tournament is a valid date'''
         try:
-            # change string input to datetime
+            # Change string input to datetime
             end_date = datetime.strptime(end_date, "%d/%m/%Y")
             
-			# check if inputted date is in the future, then not valid
-            if end_date < start_date: #LLOrganizer.choose_start_date(start_date):
+			# Check if inputted end date is after the start date
+            if end_date < start_date:
                 raise ValueError("Date has to be after the start date. Please enter a valid date.")
             return end_date
         except ValueError:
@@ -54,9 +55,9 @@ class LLOrganizer():
 
     def tournament_location(self, location: str) -> str:
         '''Checks if name is unique or missing a name'''
-        # check if name is just empty, so just space or something
+
         location = location.strip()
-        if not location:
+        if not location: # Check for empty input
             raise ValueError("Tournament location cannot be emtpy. Please enter a location.")
 
         return location
@@ -64,14 +65,14 @@ class LLOrganizer():
 
     def tournament_contact_name(self, contact_name: str) -> str:
         '''Checks if name is unique or missing a name'''
-        # check if name is just empty, so just space or something
+
         contact_name = contact_name.strip()
-        if not contact_name:
+        if not contact_name: # Check for empty input
             raise ValueError("Contact name cannot be emtpy. Please enter a contact person.")
         return contact_name
     
 
     def organizer_player_info(self):
-        """Allows the organizer to see all the players information"""
-        all_players = self.dapi.get_all_players()
+        '''Allows the organizer to see information on all the players'''
+        all_players = self.playerio.get_players()
         return all_players
