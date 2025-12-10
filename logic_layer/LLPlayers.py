57c9e2082b9c7108881f28658daccd8dca517ec4
Atlihero <atlithh25@ru.is>
2025-12-10 21:35:11 +0000

    Update LLPlayers.py

--- logic_layer/LLPlayers.py
+++ logic_layer/LLPlayers.py
@@ -124,7 +124,6 @@ class LLPlayer():
             player_obj.email,
             player_obj.handle,
             player_obj.team,
-            player_obj.points,
             player_obj.link
         ]
 
