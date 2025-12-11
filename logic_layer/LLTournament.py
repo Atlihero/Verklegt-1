94456b6a634a17c875b6a31b2e0582d79afa74aa
brynjaloa <brynjaloa@gmail.com>
2025-12-11 20:35:48 +0000

    Update LLTournament.py
    Gucci flipflop

--- logic_layer/LLTournament.py
+++ logic_layer/LLTournament.py
@@ -5,13 +5,15 @@ import random
 from datetime import datetime, timedelta
 
 
-
 class LLTournament:
     def __init__(self):
         self.data = DataAPI()
         self.teams = self.data.get_all_teams()
 
-    def create_game(self, tournament_name, round, match_number, match_date, team_a, team_b):
+    
+    def create_game(self, tournament_name, round, match_number, match_date, team_a, team_b) -> list:
+        '''Creates a game with all attributes needed for a valid game '''
+        
         return [
             tournament_name,
             round,
@@ -24,9 +26,13 @@ class LLTournament:
             None
         ]
 
-    def generate_games(self, tournament_name: str, start_date: str):
+    
+    def generate_games(self, tournament_name: str, start_date: str) -> list:
+        '''Checks if number of teams is valid and generates the rounds and generates
+        a random match schedule.'''
+        
         if len(self.teams) < 16:
-            raise ValueError("You need 16 teams")
+            raise ValueError("There need to be 16 teams. Please enter more teams")
         
         if isinstance(start_date, str):
             current_date = datetime.strptime(start_date, "%d/%m/%Y")
@@ -47,15 +53,15 @@ class LLTournament:
 
         round_teams = shuffled
 
-        for round, match_count in rounds:
+        for round, match_count in rounds: # For each round the loser team gets eliminated 
             next_round_teams = ["TBD"] * (match_count)
             for i in range(0, len(round_teams), 2):
                 team_a = round_teams[i]
                 team_b = round_teams[i + 1]
 
-                match_date = current_date.strftime("%d/%m/%Y")
+                match_date = current_date.strftime("%d/%m/%Y") # Decides when the game is set to play
 
-                game_row = self.create_game(
+                game_row = self.create_game( 
                     tournament_name = tournament_name,
                     round = round,
                     match_number = match_number,
@@ -63,35 +69,51 @@ class LLTournament:
                     team_a = team_a,
                     team_b = team_b
                 )
-                self.data.new_game(game_row)
+                self.data.new_game(game_row) # Adds the game to the game csv file
                 all_games.append(game_row)
 
                 match_number += 1
             
             round_teams = next_round_teams
-            
             current_date += timedelta(days=1)
 
         return all_games
+
     
     def update_games(self, tournament_name, match_number: int, score_a: int, score_b: int):
+        '''Updates the game based on the inputted score '''
+        
         return self.data.update_game(tournament_name, match_number, score_a, score_b)
 
-    def get_all_games(self):
+    
+    def get_all_games(self) ->:
+        '''Gets the games from the tournament'''
+        
         return self.data.get_games()
 
-    def get_all_tournamnets(self):
+    
+    def get_all_tournamnets(self) -> :
+        '''Retrieves the tournaments from the csv file'''
+        
         return self.data.get_all_tournaments()
+
     
-    def get_tournament_names(self):
-        return self.data.get_tournamentNames()
+    def get_tournament_names(self) -> list:
+        '''Retrieves the tournament names'''
+        
+        return self.data.get_tournament_names()
+
     
-    def advance(self, tournament_name: str, match_number: int, winner: str):
+    def advance(self, tournament_name: str, match_number: int, winner: str) -> :
+        '''Shows what team advances to the next round'''
+        
         return self.data.advance_round(tournament_name, match_number, winner)
     
 
-    def new_tourney(self, tournament_obj: Tournament):
-        '''want to return as a list, not a dict to have it easier to read'''
+    
+    def new_tourney(self, tournament_obj: Tournament) -> list:
+        '''Returns a list of the new tournament that was created'''
+        
         tournament_list = [
             tournament_obj.unique_name,
             tournament_obj.start_date.strftime("%d/%m/%Y"), # takes out the timestamp
