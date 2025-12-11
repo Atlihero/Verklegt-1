7f0810010d6002f8f8465f879758b6ffd22d3a2d
brynjaloa <brynjaloa@gmail.com>
2025-12-11 12:15:58 +0000

    Update Team.py
    laga bil á mili hluta og setja stóran staf fremst í komment og docstring

--- Models/Team.py
+++ Models/Team.py
@@ -1,15 +1,21 @@
 class Team:
-    #Klasi fyrir nafn, kaftein, leikmenn og ASCII logo á lið
+    '''Class for information about each team'''
     def __init__(self, name, captain, asciiLogo, wins: int = 0, points: int = 0):
         self.name = name
         self.captain = captain
         self.asciiLogo = asciiLogo
         self.wins = wins
         self.points = points
-    
-    #Strengur sem skilar þeim úpllýsingum í nýjum línum
-    def __str__(self):
-        return f"{self.name}:\n{self.captain}:\n{self.asciiLogo}:\n{self.wins}:\n{self.points}"
 
+    
+    def __str__(self):
+        '''Return the information'''
+        return (
+        f"Name      : {self.name}\n"
+        f"Captain   : {self.captain}\n"
+        f"AsciiLogo : {self.asciiLogo}"
+        )
+
+    
     def __repr__(self):
         return str(self)
