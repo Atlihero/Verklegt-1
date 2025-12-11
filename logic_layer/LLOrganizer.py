1ef9ad64c435be44d27db1b1a2199d15457b0baf
brynjaloa <brynjaloa@gmail.com>
2025-12-11 20:13:39 +0000

    Update LLOrganizer.py
    Gucci

--- logic_layer/LLOrganizer.py
+++ logic_layer/LLOrganizer.py
@@ -24,18 +24,19 @@ class LLOrganizer():
                 existing_name.append(row[0])
     
         if name in existing_name: # Checking if the name is unique
-            raise ValueError("Name already exists, please choose another one.")
+            raise ValueError("Tournament name already exists. Please choose another one.")
 
         return name
        
 
     def choose_start_date(self, start_date: str) -> datetime:
         '''Check if the start date for a tournament is a valid date'''
-        try:
+        
+		try:
             # Change string input to datetime
             start_date = datetime.strptime(start_date, "%d/%m/%Y")
             
-			# Check if inputted date is in the future, if not in the future then invalid date
+			# Check if inputted date is in the future, if date not in the future then invalid date
             if start_date < datetime.now():
                 raise ValueError("Date has to be in the future. Please enter a valid date.")
             return start_date
@@ -45,7 +46,8 @@ class LLOrganizer():
     
     def choose_end_date(self, end_date, start_date) -> datetime:
         '''Check if the end date for a tournament is a valid date'''
-        try:
+        
+		try:
             # Change string input to datetime
             end_date = datetime.strptime(end_date, "%d/%m/%Y")
             
@@ -53,12 +55,12 @@ class LLOrganizer():
             if end_date < start_date:
                 raise ValueError("Date has to be after the start date. Please enter a valid date.")
             return end_date
-        except ValueError:
+        except ValueError: # If the formatting is wrong
             raise ValueError ("Invalid date. Use DD/MM/YYYY")
     
 
     def tournament_location(self, location: str) -> str:
-        '''Checks if name is unique or missing a name'''
+        '''Checks if there is a location for the tournament'''
 
         location = location.strip()
         if not location: # Check for empty input
@@ -68,15 +70,16 @@ class LLOrganizer():
     
 
     def tournament_contact_name(self, contact_name: str) -> str:
-        '''Checks if name is unique or missing a name'''
+        '''Checks if the name is missing'''
 
         contact_name = contact_name.strip()
         if not contact_name: # Check for empty input
-            raise ValueError("Contact name cannot be emtpy. Please enter a contact person.")
+            raise ValueError("Contact name cannot be emtpy. Please enter a name for the contact person.")
         return contact_name
     
 
     def organizer_player_info(self):
         '''Allows the organizer to see information on all the players'''
-        all_players = self.playerio.get_players()
+        
+		all_players = self.playerio.get_players()
         return all_players
