ebfbe9c3da448e1f7554e36e93c8fd81a7e8cb0a
brynjaloa <brynjaloa@gmail.com>
2025-12-11 12:19:49 +0000

    Update Round.py
    laga bil á mili hluta og setja stóran staf fremst í komment og docstring

--- Models/Round.py
+++ Models/Round.py
@@ -5,13 +5,22 @@ class Round(Tournament):
     the round is and when it starts and ends'''
     def __init__(self, number, startTime, endTime):
         self.number = number
-        self.startTime = startTime
-        self.endTime = endTime
+        self.startTime = start_time
+        self.endTime = end_time
 
 
     def __str__(self):
         '''Returns the information'''
-        return f"{self.name}:\n{self.number}:\n{self.startTime}:\n{self.endTime}"
+        return f"{self.name}:\n{self.number}:\n{self.start_time}:\n{self.end_time}"
+
+    def __str__(self):
+        '''Return the information'''
+        return (
+        f"Name: {self.name}\n"
+        f"Number: {self.number}\n"
+        f"Start Time: {self.start_time}\n"
+        f"End Time: {self.end_time}"
+        )
 
     
     def __repr__(self):
