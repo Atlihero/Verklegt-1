51bbe9e7674869bdd8547b8e2cd345ef7bc4d5b6
brynjaloa <brynjaloa@gmail.com>
2025-12-11 12:08:41 +0000

    Update Result.py
    laga bil á mili hluta og setja stóran staf fremst í komment og docstring

--- Models/Result.py
+++ Models/Result.py
@@ -1,11 +1,14 @@
 from Models.Game import Game
 
 class Result(Game):
+    '''Class that keeps track of the results of each round'''
     def __init__(self, score_A, score_B, winner):
         super().__init__()
         self.score_A = score_A
         self.score_B = score_B
         self.winner = winner
 
+    
     def __str__(self):
+        '''Return the information'''
         return f"{self.score_A}:\n{self.score_B}:\n{self.winner}"
